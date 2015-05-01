3949461b757735ef06750f8fe81e2d542aeca9f4
Sharon Siegel <sjsieg830@gmail.com>
2015-05-01 12:25:50 -0600

    App now displays current temp/conditions and 5 day forecast based on zip code input by user.

--- weather.rb
+++ weather.rb
@@ -10,3 +10,17 @@ current_temp = response.condition.temp
 current_conditions = response.condition.text
 
 puts "The current temperature is #{current_temp} and it is #{current_conditions}."
+
+(0..4).each do |n|
+    forecast_day = response.forecasts[n].day
+    forecast_condition = response.forecasts[n].text
+    forecast_hi = response.forecasts[n].high
+    forecast_low = response.forecasts[n].low
+    if n == 0
+        puts "Today's forecast is #{forecast_condition} with a high of #{forecast_hi} and a low of #{forecast_low}."
+    elsif n == 1
+        puts "Tomorrow's forecast is #{forecast_condition} with a high of #{forecast_hi}and a low of #{forecast_low}."
+    else
+        puts "#{forecast_day}'s forecast is #{forecast_condition} with a high of #{forecast_hi} and a low of #{forecast_low}"
+    end
+end
